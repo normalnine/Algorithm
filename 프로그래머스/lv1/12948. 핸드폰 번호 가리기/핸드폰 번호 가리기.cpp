34c1c7c16f3f476474d9a9f5a9075d5981efccca
normalnine <codejamgg@gmail.com>
2023-06-13 09:19:31 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    
+    for(int i = 0; i<phone_number.size()-4; i++)
+    {
+        phone_number[i] = '*';
+    }
+    
+    return phone_number;
+}

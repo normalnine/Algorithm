8b23c4aad5f8a978f89c495620cb0ba9bf5a1e54
normalnine <codejamgg@gmail.com>
2023-06-13 09:32:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.cpp
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.cpp
@@ -4,19 +4,6 @@
 using namespace std;
 
 int solution(vector<int> absolutes, vector<bool> signs) {
-    int answer = 0;
-    
-    for(int i = 0; i<absolutes.size(); i++)
-    {
-        if(signs[i])
-        {
-            answer+=absolutes[i];
-        }
-        else
-        {
-            answer-=absolutes[i];
-        }
-    }
-    
+    int answer = 123456789;
     return answer;
 }

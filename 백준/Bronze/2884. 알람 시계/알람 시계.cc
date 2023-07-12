cdd3d48ea580b6e4f6d6cb004e96bac74958c53e
normalnine <codejamgg@gmail.com>
2023-07-12 12:31:15 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int h, m;
+    
+    cin>>h>>m;
+    
+    if(m-45<0)
+    {
+        h--;
+        m = m-45+60;
+        
+        if(h<0)
+        {
+            h = 23;
+        }
+    }
+    else
+    {
+        m-=45;
+    }
+    
+    cout<<h<<" "<<m;
+    
+    
+    return 0;
+}

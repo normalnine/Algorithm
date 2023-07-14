acd6ea29d998c79fc9d9fa796b74a7524cd99cb0
normalnine <codejamgg@gmail.com>
2023-07-14 13:40:05 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int count, a, b;
+    
+    cin>>count;
+    
+    for(int i = 0; i<count; i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    
+    return 0;
+}

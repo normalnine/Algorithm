471db003b3cb6dc8b08ea975f4d12d4341e115b8
normalnine <codejamgg@gmail.com>
2023-08-20 10:47:38 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    
+    for(int i=0; i<5; i++)
+    {
+        cin>>n;
+        
+        sum += n*n;
+    }
+    
+    sum%=10;
+    
+    cout<<sum;
+
+    return 0;
+}

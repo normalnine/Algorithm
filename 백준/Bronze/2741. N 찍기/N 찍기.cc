b7b6a39d3ed9d3ef0e404e088ee8f90c21576f7d
normalnine <codejamgg@gmail.com>
2023-08-21 13:22:42 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    
+    cin>>num;
+    
+    for(int i=1; i<=num; i++)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

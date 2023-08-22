8aa8bc59802df3cfd3c95dfd864df79fd5404fac
normalnine <codejamgg@gmail.com>
2023-08-22 13:30:36 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    int num;
+    int arr[10] = {};
+    
+    cin>>a>>b>>c;
+    
+    num = a*b*c;
+    while(num>0)
+    {
+        arr[(num%10)]++;
+        num/=10;
+    }
+    
+    for(int i=0; i<10; i++)
+    {
+        cout<<arr[i]<<"\n";
+    }
+
+    
+    return 0;
+}

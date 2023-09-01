a79d8b45be2d79263570409aeb074dfe03bb39e6
normalnine <codejamgg@gmail.com>
2023-09-01 13:07:48 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -6,14 +6,15 @@ int main()
 {
     int n;
     char s[51];
-    long long h = 0;
+    long long h = 0, r=1, m = 1234567891;
     
     cin>>n;
     
     for(int i=0; i<n; i++)
     {
         cin>>s[i];
-        h += (s[i]-96) * pow(31, i);
+        h += ((s[i]-96) * r) % m;
+        r =  r * 31 % m;
     }
     
     cout<<h;

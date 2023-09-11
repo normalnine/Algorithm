cfe19159775c16630cac96d68ac0dbdf93af4b59
normalnine <codejamgg@gmail.com>
2023-09-11 13:03:55 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    
+    cin>>n>>m;
+    
+    if(n-m < 0)
+    {
+        cout<<-(n-m);
+    }
+    else
+    {
+        cout<<(n-m);
+    }
+
+    return 0;
+}

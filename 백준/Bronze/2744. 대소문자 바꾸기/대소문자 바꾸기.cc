4993e3f0b54c712775d303d1658e77276766f2fb
normalnine <codejamgg@gmail.com>
2023-09-27 22:17:33 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    
+    cin>>s;
+    
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i]>96)
+        {
+            s[i]-=32;
+        }
+        else
+        {
+            s[i]+=32;
+        }
+    }
+    
+    cout<<s;
+    
+    return 0;
+}

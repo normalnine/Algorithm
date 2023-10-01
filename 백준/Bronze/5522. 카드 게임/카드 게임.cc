9d1f69c6ae6dbbfaf5a8271cc971f0de198796bf
normalnine <codejamgg@gmail.com>
2023-10-02 00:31:36 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    
+    for(int i=0; i<5; i++)
+    {
+        cin>>n;
+        sum+=n;
+    }
+    
+    cout<<sum;
+    
+    return 0;
+}
